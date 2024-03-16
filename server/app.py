69e57eea5b49bec2168dfc000b116ba3551a6ed7
Kevin <kevin.kiseli@student.moringaschool.com>
2024-03-16 11:13:05 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
+
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
@@ -20,19 +21,69 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+       bakery_dict = {
+           'id': bakery.id,
+           'name': bakery.name,
+           "created_at":bakery.created_at,
+           "updated_at":bakery.updated_at,
+       }
+       bakeries.append(bakery_dict)
+
+    response = make_response(
+           jsonify(bakeries),
+           200
+       )
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+           jsonify(bakery_dict),
+           200
+       )
+    response.headers["Content-Type"] = "application/json"
+    return response
+    
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all():
+       baked_good_dict = {
+           'id': baked_good.id,
+           'name': baked_good.name,
+           "price": baked_good.price,
+           "created_at": baked_good.created_at,
+           "updated_at": baked_good.updated_at,
+       }
+       baked_goods.append(baked_good_dict)
+
+    response = make_response(
+           jsonify(baked_goods),
+           200
+       )
+    response.headers["Content-Type"] = "application/json"
+    return response
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    baked_good_dict = baked_good.to_dict()
+
+    response = make_response(
+           jsonify(baked_good_dict),
+           200
+       )
+    response.headers["Content-Type"] = "application/json"
+    return response
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
